c9adb55056362a96a81d35ae972342b824a2c36c
Siti Khotijah <53899191+skhotijah@users.noreply.github.com>
2021-09-30 21:44:15 +0700

    Create Switch_case.cpp

---
+++ Switch_case.cpp
@@ -0,0 +1,25 @@
+#include<iostream> 
+using namespace std;
+int main()
+{
+	int x = 20;
+	switch (x)
+	{
+	case 10: 
+		cout<<"X is 10"; break;
+
+	case 20: 
+		cout << "X is 20"; break;
+
+	case 30: 
+		cout << "X is 30"; break;
+
+	default: 
+		cout<<"X is not 10, 20 or 30"; break;
+
+	}
+	return 0;
+}
+
+
+
