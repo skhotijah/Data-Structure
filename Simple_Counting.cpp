c6e6486a66880accd87f07aa756a8217832971e2
Siti Khotijah <53899191+skhotijah@users.noreply.github.com>
2021-09-30 21:47:21 +0700

    Create Simple_Counting.cpp

---
+++ Simple_Counting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a, b, c;
+
+    cout << "Input a: ";
+    cin >> a;
+    cout << "Input b: ";
+    cin >> b;
+
+    // operator 
+    c = a + b;
+
+    cout << "Result a + b = " << c << endl;
+
+    return 0;
+}
